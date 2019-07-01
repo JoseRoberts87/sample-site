16a09aec784b28f663a45492b40cf7b37e0d3e0c
JoseRoberts87 <robertslafamilia@outlook.com>
2019-06-30 20:11:33 -0500

    hyperlinks wrk

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -8,26 +8,26 @@ function config($locationProvider, $routeProvider) {
             controller: homeController,
             controllerAs: 'vm'
         })
-        .when('/contact', {
-            templateUrl: '/angular-app/contact/contact.html',
-            controller: contactController,
-            controllerAs: 'vm'
-        })
-        .when('/about', {
-            templateUrl: '/angular-app/about/about.html',
-            controller: aboutController,
-            controllerAs: 'vm'
-        })
-        .when('/services', {
-            templateUrl: '/angular-app/services/services.html',
-            controller: servicesController,
-            controllerAs: 'vm'
-        })
-        .when('/:str', {
-            templateUrl: '/angular-app/home/home.html',
-            controller: searchController,
-            controllerAs: 'vm'
-        })
+        // .when('/contact', {
+        //     templateUrl: '/angular-app/contact/contact.html',
+        //     controller: contactController,
+        //     controllerAs: 'vm'
+        // })
+        // .when('/about', {
+        //     templateUrl: '/angular-app/about/about.html',
+        //     controller: aboutController,
+        //     controllerAs: 'vm'
+        // })
+        // .when('/services', {
+        //     templateUrl: '/angular-app/services/services.html',
+        //     controller: servicesController,
+        //     controllerAs: 'vm'
+        // })
+        // .when('/:str', {
+        //     templateUrl: '/angular-app/home/home.html',
+        //     controller: searchController,
+        //     controllerAs: 'vm'
+        // })
         .otherwise({
             redirectTo: '/'
         })
